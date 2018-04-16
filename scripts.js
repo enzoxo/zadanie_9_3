464fbdb7883f33dde7c868e57e7399aa4e7082aa
Michal Bandyszak <michalbandyszak@gmail.com>
2018-04-17 00:02:25 +0200

    Change script.js -> variable instead of number.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-var text="Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
 var dinosaur ="triceratops",
-	dinosaurUpper = dinosaur.toUpperCase();
+	dinosaurUpper = dinosaur.toUpperCase(),
+	lengthOfText;
 
 text = text.replace("Velociraptor", dinosaurUpper);
 console.log(text.length);
-console.log(text.slice(0,72));
+lengthOfText = text.length;
+console.log(text.slice(0,lengthOfText/2));
